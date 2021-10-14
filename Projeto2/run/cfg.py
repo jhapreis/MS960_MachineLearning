7fcbb1b2172f72bb8993832b93589fb889fcdae1
Jorge Reis <63481188+jhapreis@users.noreply.github.com>
2021-10-14 14:49:54 -0300

    Update cfg.py

--- Projeto2/run/cfg.py
+++ Projeto2/run/cfg.py
@@ -4,15 +4,15 @@
 labels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] # official labels
 
 number_of_layers  = 3 # start, middle(hidden), end
-mult_hidden_layer = 4 # n times the start layer 
-additional_layers = 0 #   
+mult_hidden_layer = 0 # n times the start layer 
+additional_layers = 25 #   
 
 init_thetas_range = 1E-1 # range to initiate the thetas constants
 
 lambda_value      = 1E-1 # lambda for the regularization
 
 max_cost          = 1E-2      
-max_tries         = int(1E4)  
+max_tries         = int(1E1)  
 learning_rate     = 1E-1 
 
 tracking          = True
